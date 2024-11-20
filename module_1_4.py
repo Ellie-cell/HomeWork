83e2a94c232a81cf6b2dbff576aa9fb18ad573d5
Ellie-cell <hinashoua2000@gmail.com>
2024-11-20 16:03:48 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input(("Введите Ваш адрес проживания "))
+print(('Ваш адрес проживания,') , my_string)
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+
