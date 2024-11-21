97b5137d0ed48f6c64dce6557c136362f95ef766
Ellie-cell <hinashoua2000@gmail.com>
2024-11-21 09:08:24 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1,1.5,'sammer',False
+print(immutable_var)
+#immutable_var[0]= 100
+#Кортеж относится к неизменяемым типам данных
+mutable_list = [1,1.5,'Sammer', False]
+print(mutable_list)
+mutable_list[0] = 'Forever'
+print(mutable_list)
