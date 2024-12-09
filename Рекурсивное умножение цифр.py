64e68c3ae6851f8690fbaed4bd5b14aaf7d4c503
Ellie-cell <hinashoua2000@gmail.com>
2024-12-09 17:10:17 +0700

    Update Рекурсивное умножение цифр.py

--- Рекурсивное умножение цифр.py
+++ Рекурсивное умножение цифр.py
@@ -2,15 +2,20 @@ def get_multiplied_digits(number) :
     str_number = str(number)
 
     if len(str_number) == 1 :
-        return int(str_number)
+        last_number = int(str_number)
 
-    else:
+        if last_number == 0:
+            return 1
+        else :
+            return last_number
+
+
+    else :
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
 
 result = get_multiplied_digits(40203)
 print(result)
 
-result2 = get_multiplied_digits(4020301)
+result2 = get_multiplied_digits(402030)
 print(result2)
-
